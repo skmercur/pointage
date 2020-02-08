d56181bdda962fb6f2b65e9e752da7d54cb70c8c
sofiane khoudour <khoudoursofiane75@gmail.com>
2020-02-08 19:01:45 +0100

    Updates

---
+++ precache-manifest.26c29129a7d4938d414c02c3c3a9aef9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d086c25c877b9ac10c1581f581aa8ead",
+    "url": "/pointage/index.html"
+  },
+  {
+    "revision": "3f292b3683ea9c9750a2",
+    "url": "/pointage/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1ad22942458ddd88b63a",
+    "url": "/pointage/static/js/2.8af15a1b.chunk.js"
+  },
+  {
+    "revision": "89e606ebb144d5268015dbf56bb3a52c",
+    "url": "/pointage/static/js/2.8af15a1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f292b3683ea9c9750a2",
+    "url": "/pointage/static/js/main.3bb823a0.chunk.js"
+  },
+  {
+    "revision": "748e4e43a7b69140140e",
+    "url": "/pointage/static/js/runtime-main.78321572.js"
+  }
+]);
